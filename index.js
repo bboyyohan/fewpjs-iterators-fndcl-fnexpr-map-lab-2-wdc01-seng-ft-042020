77a25cacca43722ea3523fb79454f844b36332ad
github username <github email address>
2020-06-01 16:49:54 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const questions = [
 ];
 
 const capitalizeSentence = () => {
-  return questions
+  return questions.map(function(sentence) {
+    let firstWord = sentence[0].toUpperCase()
+    return firstWord + sentence.slice(1)
+    // debugger
+  }
+  )
 }
